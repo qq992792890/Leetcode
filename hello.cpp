21bce7ed72ee47bde413bfd686af54d00128646d
michael <qq992792890@163.com>
2018-03-27 22:05:28 +0800

    change a little

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 int main(){
-    std::cout<<"Hello,this is a test file\n";
+    std::cout<<"Hello,this is a test file\n please input an number:";
+    int var=0;
+    std::cin>>var;
+    std::cout<<var<<std::endl;
     return 0;
 }
