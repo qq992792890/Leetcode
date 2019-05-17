a85e36450761783278ed114ed145a8036b34ea79
MichaelPC <qq992792890@163.com>
2019-05-17 19:32:45 +0800

    pass twoSum 1 8ms 9.8MB

--- array/twosum_1.cpp
+++ array/twosum_1.cpp
@@ -40,6 +40,53 @@ public:
     }  
 };
 
+class MySolution {
+    public:
+    vector<int> twoSum(vector<int> &nums,int target){
+        vector<int> res;
+        if (nums.size()<2)
+        {
+            return res;
+        }
+        vector<int> newnums(nums);
+        sort(newnums.begin(),newnums.end());//define new vector to store the sorted nums
+        int i,j;
+        i=0;
+        j=newnums.size()-1;
+        while (i<j)
+        {
+            if (newnums[i]+newnums[j]>target)
+            {
+                --j;
+            }else if (newnums[i]+newnums[j]<target)
+            {
+                ++i;
+            }else {
+                
+                break;
+                }
+            
+            
+        }
+        for (int k = 0; k < nums.size(); ++k)
+        {
+            if (nums[k]==newnums[i])
+            {
+                res.push_back(k);
+                continue; 
+            }
+            if (nums[k]==newnums[j])
+            {
+                res.push_back(k);
+                continue; 
+            }
+        }
+        
+        return res;
+        
+    }
+};
+
 int main(int argc, char const *argv[])
 {
     /* code */
@@ -48,7 +95,8 @@ int main(int argc, char const *argv[])
     int target=9;
     // cout<<target<<"\n"<<nums[2]<<"\n";
     vector<int> res;
-    Solution s;
+    // Solution s;
+    MySolution s;//check if my solution work
     res=s.twoSum(nums,target);
     cout<<"answer is:\n";
     for (int i = 0; i < res.size(); ++i)
