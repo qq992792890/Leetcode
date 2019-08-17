01f8a5759f3920372fe3095743fb96d6e190941d
qq992792890 <qq992792890@163.com>
2019-08-17 11:23:22 +0800

    add 26 27 28

---
+++ 28.实现-str-str.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=cpp
+ *
+ * [28] 实现 strStr()
+ */
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle == "")
+        {
+            return 0;
+        }
+        if (neddle.length() > haystack.length())
+        {
+            return -1;
+        }
+        for (int i = 0; i < haystack.length(); i++)
+        {
+            //未完成
+        }
+        
+        
+        
+    }
+};
+
