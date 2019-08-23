98c3bcbc34ea557e97e2b68784247cb518169653
qq992792890 <qq992792890@163.com>
2019-08-23 21:14:26 +0800

    modify somefiles

--- 6.z-字形变换.cpp
+++ 6.z-字形变换.cpp
@@ -6,6 +6,57 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
+        int size_s = s.size(), pos = 0, i = 0;
+        vector<string> res;
+        string str0;
+        int end = 0;
+
+        //problems unsolved now ,waiting to be done 
+        for (int j = 0; j < numRows; j++)
+        {
+            res.push_back(str0);
+        }
+        
+        while (pos < size_s)
+        {
+            if (pos + numRows > size_s)
+            {
+                end = size_s - pos;
+            }else
+            {
+                end = numRows;
+            }
+            
+            
+            for (i = 0; i < end; i++)
+            {
+                res[i].push_back(s[pos]);
+                pos++;
+            }
+            //i = numRows - 1;
+
+            if (pos + numRows - 2 > size_s)
+            {
+                end = size_s - pos;
+            }
+            else
+            {
+                end = numRows;
+            }
+            for (i = numRows - 2; i > numRows - 2 - end; i--)
+            {
+                res[i].push_back(s[pos]);
+                pos++;
+            }
+
+        }
+        
+        string result;
+        for (int k = 0; k < numRows; k++)
+        {
+            result += res[k];
+        }
+        return result;
         
     }
 };
--- Hardcore.md
+++ Hardcore.md
@@ -1,6 +1,8 @@
 # Some HardCore Problems
 [TOC]
 
+未完成题目  有些难度（还是从简单题开始入手比较好）
+5.最长回文子串  6.z-字形变换
 ## [15.Three Sum](https://leetcode-cn.com/problems/3sum/)
 
 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
