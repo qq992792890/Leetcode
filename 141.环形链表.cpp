ff3f876641a942451b225af127222be67b2b5f52
qq992792890 <qq992792890@163.com>
2019-09-03 21:23:30 +0800

    141

---
+++ 141.环形链表.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=cpp
+ *
+ * [141] 环形链表
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (/* condition */)
+        {
+            /* code */
+        }
+        
+        
+    }
+};
+
