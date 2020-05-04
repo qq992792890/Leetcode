0e5d24be5ffc2c8f25086555b2684674aecd16ba
MichaelPC <qq992792890@163.com>
2020-05-05 00:46:09 +0900

    53 和 45 5月打卡3日和4日份

---
+++ 45.跳跃游戏-ii.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=45 lang=cpp
+ *
+ * [45] 跳跃游戏 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        //看到困难的题就，嗯。。。有借鉴了
+        int maxPos = 0, n = nums.size(), end = 0, step = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (maxPos >= i) {
+                maxPos = max(maxPos, i + nums[i]);
+                if (i == end) {
+                    end = maxPos;
+                    ++step;
+                }
+            }
+        }
+        return step;
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    }
+};
+// @lc code=end
+
--- 53.最大子序和.cpp
+++ 53.最大子序和.cpp
@@ -16,86 +16,99 @@ public:
 
         该方法可以改进，因为最大子串可能
         */
-        int left = 0, right = 0;
-        int maxpos = 0;//向右最大正子集 //必须有初始化，否则会内存溢出
-        int minneg = 0;//左边小负子集
-        int sum = 0,sumright = 0, sumleft = 0;
-        int size_nums = nums.size();
-        int i = 0;
-        int poscount = 0;//计算所有正数个数
-        if (size_nums == 1)//2.only one digit in list
-        {
-            return nums[0];
+//时在没想到这么简洁就可以算出来！！
+        int pre = 0, maxAns = nums[0];
+        for (const auto &x: nums) {
+            pre = max(pre + x, x);
+            maxAns = max(maxAns, pre);
         }
+        return maxAns;
 
-        //initial
-        sumright = nums[0];
-        maxpos = 0;
-        for (i = 0; i < size_nums; i++)
-        {
-            sum += nums[i];
-            if (sum > sumright)
-            {
-                sumright = sum;
-                maxpos = i;
-            }
-            if (nums[i] > 0)//count positive digit
-            {
-                poscount ++;
-            }
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/maximum-subarray/solution/zui-da-zi-xu-he-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+        // int left = 0, right = 0;
+        // int maxpos = 0;//向右最大正子集 //必须有初始化，否则会内存溢出
+        // int minneg = 0;//左边小负子集
+        // int sum = 0,sumright = 0, sumleft = 0;
+        // int size_nums = nums.size();
+        // int i = 0;
+        // int poscount = 0;//计算所有正数个数
+        // if (size_nums == 1)//2.only one digit in list
+        // {
+        //     return nums[0];
+        // }
+
+        // //initial
+        // sumright = nums[0];
+        // maxpos = 0;
+        // for (i = 0; i < size_nums; i++)
+        // {
+        //     sum += nums[i];
+        //     if (sum > sumright)
+        //     {
+        //         sumright = sum;
+        //         maxpos = i;
+        //     }
+        //     if (nums[i] > 0)//count positive digit
+        //     {
+        //         poscount ++;
+        //     }
             
             
-        }
-        if (sumright > 0)
-        {
-            right = maxpos;
-        }else if (i >= size_nums)
-        {//if biggest sumright no bigger than 0 and check to the end
-            right = maxpos;
-        }//there might be all the digit in the list less than 0
-        sum = nums[0];
-        if (poscount <= 0)//3.third changes
-        {
-            for (int k = 0; k < size_nums; k++)
-            {
-                if (nums[k] > sum)
-                {
-                    sum = nums[k];
-                }
+        // }
+        // if (sumright > 0)
+        // {
+        //     right = maxpos;
+        // }else if (i >= size_nums)
+        // {//if biggest sumright no bigger than 0 and check to the end
+        //     right = maxpos;
+        // }//there might be all the digit in the list less than 0
+        // sum = nums[0];
+        // if (poscount <= 0)//3.third changes
+        // {
+        //     for (int k = 0; k < size_nums; k++)
+        //     {
+        //         if (nums[k] > sum)
+        //         {
+        //             sum = nums[k];
+        //         }
                 
-            }
-            return sum;//return the biggedt negtive digit
+        //     }
+        //     return sum;//return the biggedt negtive digit
             
-        }
+        // }
         
         
         
-        right = maxpos;
-        sum = 0;
-        sumleft = nums[0];
-        minneg = 0;
-        for (int j = 0; j < right; j++)
-        {
-            sum += nums[j];
-            if (sum < sumleft)
-            {
-                sumleft = sum;
-                minneg = j;
-            }
+        // right = maxpos;
+        // sum = 0;
+        // sumleft = nums[0];
+        // minneg = 0;
+        // for (int j = 0; j < right; j++)
+        // {
+        //     sum += nums[j];
+        //     if (sum < sumleft)
+        //     {
+        //         sumleft = sum;
+        //         minneg = j;
+        //     }
             
-        }
-        if (sumleft < 0)
-        {
-            left = minneg + 1;
-        }
+        // }
+        // if (sumleft < 0)
+        // {
+        //     left = minneg + 1;
+        // }
 
-        sum = 0;
-        for (int i = left; i <= right; i++)
-        {
-            sum += nums[i];
-        }
+        // sum = 0;
+        // for (int i = left; i <= right; i++)
+        // {
+        //     sum += nums[i];
+        // }
         
-        return sum;
+        // return sum;
         
         
 
